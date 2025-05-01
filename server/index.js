577df18a71646eb3198425f6ac6721c2549a1670
Shilpi Saha <sahashilpi3099@gmail.com>
2025-05-01 20:22:52 +0530

    api-creation

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express=require("express");
+var server=express();
+
+const mongoose=require("mongoose");
+const router=express.Router();
+
+const cors = require("cors");
+server.use(express.json());
+server.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/proj_db").then(() => {
+  console.log("connected succes");
+});
+
+const empData=new mongoose.Schema({
+name:{type:String},
+email:{email:String}
+})
+
+const empModel=mongoose.model("empData",empData)
+router.post("/sendData",async(req,res)=>{
+    try{
+    const {name,email}=req.body;
+        const empData=await empModel.create({name,email});
+        res.status(201).send(empData);
+    }
+    catch(err){
+        res.status(500).send();
+    }
+})
+
+server.use(router);
+server.listen(8080,()=>{
+console.log("8080")
+})
---
+++ server/routes/routes.js
@@ -0,0 +1,7 @@
+// const express=require('express');
+
+// const router = express.Router();
+
+// router.post("/send-Data",async(req,res)=>{
+//     const {name}=req.body;
+// })
